ef61010108f1aabe6f6ebcf2a3945fbf0951db47
Jeff Lin <cptl.lin@gmail.com>
2021-07-29 02:34:06 +0800

    The second commit

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.example.bmi
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.bmi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // This is test1
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        b_help.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setMessage("You click the HELP")
+                .setTitle("Toast")
+                .setPositiveButton("OK", null)
+                .show()
+        }
+    }
+
+    fun bmi(view: View) {
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("BMI", bmi.toString())
+
+        Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle("Your BMI")
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
